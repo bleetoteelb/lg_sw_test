6700b7ecb12dc89dbbd3fbb8c443ac4e43354d3a
박소환/연구원/TV Multimedia Solution팀(sohwan.park@lge.com) <sohwan.park@lge.com>
2019-04-10 19:41:04 +0900

    update

--- sample_product_picture_14/main.cpp
+++ sample_product_picture_14/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int N;//제품 수
 struct st{
 	int X, ID;//위치, 아이디
-    st(int x,int id):X(x),ID(id){}
+	int newid;
+    st(int x,int id,int ni):X(x),ID(id),newid(ni){}
 };
 //st A[50010];
 
@@ -16,16 +17,29 @@ bool compare(st a, st b){
 
 //priority_queue<st,vector<st>,compare> A;
 vector<st> A;
-int rearr[50010]={0,};
+
 int check[50010]={0,};
-int check_cnt=0;
+int check_cnt=1;
 
 void InputData(){
 	cin >> N;
-    int x, id;
+    int x, id,tmp;
+
 	for (int i = 0; i < N; i++){
         scanf("%d %d",&x,&id);
-        A.push_back(st(x,id));
+		tmp = id % 50000;
+		for(int j=0;j<50000;j++){
+			if(check[tmp]==0){
+				check[tmp] = id;
+				A.push_back(st(x,id,tmp));
+				break;
+			}else if(check[tmp]==id){
+				A.push_back(st(x,id,tmp));
+				break;
+			}
+			tmp = (tmp+1) % 50000;
+		}
+        A.push_back(st(x,id,tmp));
 		//cin >> A[i].X >> A[i].ID;
 	}
     sort(A.begin(),A.end(),compare);
@@ -38,7 +52,7 @@ int main(){
 
 	//	코드를 작성하세요
     int size=A.size();
-	for(int i=0;i<size;i++) printf("%d %d\n",A[i].X,A[i].ID);
+	for(int i=0;i<size;i++) printf("%d %d %d\n",A[i].X,A[i].ID,A[i].newid);
 	
 	cout << ans << endl;	//	정답 출력
 	return 0;
